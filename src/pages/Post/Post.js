d1baf31f412e9ce7e8b348e41ed0ea172be18277
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-06-23 03:16:39 -0300

    Post page added

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { format } from 'date-fns';
+import ReactMarkdown from 'react-markdown';
 
-import api from '../../services/api';
+import Clap from 'react-clap-button';
 
-import { Container, PostHeader } from './PostStyle';
+import api, { baseURL } from '../../services/api';
+import Codeblock from '../../components/Codeblock/Codeblock';
+
+import { Container, PostHeader, UserInfo, MarkdownContainer, ClapContainer } from './PostStyle';
 
 class Post extends Component {
   state = {
     post: {},
+    loading: true,
+    error: null,
   };
 
   async componentDidMount() {
@@ -14,17 +21,53 @@ class Post extends Component {
       const { id } = this.props.match.params;
       const { data: post } = await api.get(`/posts/${id}`);
       this.setState({ post });
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+    }
   }
+
+  onClap = async () => {
+    try {
+      const { id } = this.props.match.params;
+      const { data: post } = await api.post(`/posts/${id}/like`);
+      this.setState({
+        post: {
+          ...this.state.post,
+          claps: this.state.post.claps + 1,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   render() {
     const { post } = this.state;
     return (
       <Container>
-        <PostHeader>
-          <h1>{post.title}</h1>
-          <p>{post.description}</p>
-        </PostHeader>
+        <ClapContainer onClick={this.onClap}>{post && <Clap count={post.claps} />}</ClapContainer>
+        {post && (
+          <PostHeader>
+            <h1>{post.title}</h1>
+            <p>{post.description}</p>
+            {post.author && (
+              <UserInfo>
+                <img src={`${baseURL}/files/${post.author.avatar}`} alt="" />
+                <div>
+                  <span>
+                    {post.author && post.author.name}
+                    <small>Follow</small>
+                  </span>
+                  <small>{format(post.createdAt, 'MMM D')}</small>
+                </div>
+              </UserInfo>
+            )}
+            <img src={`${baseURL}/files/${post.featured_img}`} alt="" />
+          </PostHeader>
+        )}
+        <MarkdownContainer>
+          <ReactMarkdown source={post.text} renderers={{ code: Codeblock }} />
+        </MarkdownContainer>
       </Container>
     );
   }
--- src/pages/Post/PostStyle.js
+++ src/pages/Post/PostStyle.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 70%;
+  width: 55%;
   margin: 3rem auto;
 `;
 
 export const PostHeader = styled.div`
-  width: 80%;
-  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
   h1 {
     margin: 0;
     font-family: 'Lora', serif;
@@ -21,4 +21,77 @@ export const PostHeader = styled.div`
     font-size: 24px;
     color: #757575;
   }
+
+  img {
+    width: auto;
+    height: 400px;
+    max-height: 400px;
+  }
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 2rem;
+
+  img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+  }
+
+  div {
+    margin-left: 12px;
+    display: flex;
+    flex-direction: column;
+
+    small {
+      font-size: 12px;
+      color: #757575;
+    }
+
+    span {
+      font-size: 14px;
+      color: #424242;
+      text-transform: capitalize;
+
+      small {
+        color: #008e00;
+        margin-left: 12px;
+        border: 1px solid #008e00;
+        padding: 0 4px;
+        border-radius: 4px;
+        width: 50px;
+        display: inline-block;
+        text-align: center;
+        font-size: 13px;
+
+        &:hover {
+          cursor: pointer;
+          opacity: 0.8;
+        }
+      }
+    }
+  }
+`;
+
+export const MarkdownContainer = styled.div`
+  font-family: 'Lora', serif;
+  color: #272a2b;
+
+  p {
+    font-size: 20px;
+    line-height: 30px;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: underline;
+  }
+`;
+
+export const ClapContainer = styled.div`
+  position: fixed;
+  left: 120px;
+  top: 40%;
 `;
